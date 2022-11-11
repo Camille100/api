147564704512e0d9e2cf0add3d973de999de2817
Camille100 <76488507+Camille100@users.noreply.github.com>
2022-11-11 22:51:29 +0100

    authentication CRUD

---
+++ controllers/authController.js
@@ -0,0 +1,112 @@
+/* eslint-disable no-unused-vars */
+import dotenv from 'dotenv';
+import User from '../models/userModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import jwtDecode from 'jwt-decode';
+
+dotenv.config();
+
+export const generateAccessToken = (user) => {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '600s' });
+}
+
+export const register = async (req, res) => {
+    if (req.body.email === undefined || req.body.password == undefined) {
+        return res.status(400).json({ error: 'Missing informations' });
+    }
+    if (req.body.password.length < 6) {
+        return res.status(400).json({ error: 'Password too short' });
+    }
+
+    User.findOne({ email: req.body.email })
+    .lean()
+    .exec(async (error, user) => {
+      console.log(user);
+      if (error) return res.status(400).json(error);
+      else if (user === null) {
+        const password = await bcrypt.hash(req.body.password, 10);
+        const user = new User({
+            pseudo: req.body.pseudo,
+            email: req.body.email,
+            password,
+            role: 'user',
+            xp: 0,
+        })
+        user.save()
+            .then((response) => {
+              return res.status(201).json(response);
+            })
+      } else {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    })
+}
+
+export const login = (req, res) => {
+    User.findOne({ email: req.body.email })
+    .lean()
+    .exec((error, user) => {
+        if (error) return res.status(400).json(error);
+        if (user === null) return res.status(404).json({error: 'Not found'});
+
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+            if (err) return res.status(401).json(err);
+            if (result === true) {
+            const id = user._id.toString();
+            const userInfos = { id };
+            const accessToken = generateAccessToken(userInfos);
+            const refreshToken = jwt.sign(userInfos, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '365d' });
+            
+            User.updateOne({ _id: user._id }, { $set: { refreshToken: refreshToken }})
+              .lean()
+              .exec((errorUpdate, update) => {
+                if (errorUpdate) return res.status(400).json(errorUpdate);
+                const auth = { accessToken };
+
+                return res
+                .status(200)
+                .cookie('refreshToken', refreshToken, { httpOnly: true, secure: true, sameSite: 'strict', expires: new Date(Date.now() + 60 * 365 * 60000) })
+                .json({
+                  email: user.email,
+                  pseudo: user.pseudo,
+                  id: user._id,
+                  role: user.role,
+                  ...auth
+                });
+              })
+            } else {
+              return res.status(401).json({error: 'Unauthorized'});
+            }
+        })
+    })
+}
+
+export const refreshToken = (req, res) => {
+  let refreshToken = req.cookies.refreshToken;
+  console.log(refreshToken);
+  const id = jwtDecode(refreshToken).id;
+  if (refreshToken === null) return res.status(401).json({ error: 'Unauthorized' });
+  console.log('bloup1');
+  User.findOne({ _id: id })
+  .exec((err, userFound) => {
+      console.log(userFound);
+    if (err) return res.status(400).json(err);
+    if (userFound === null) return res.status(401).json({ error: 'Unauthorized' });
+    console.log('bloup2');
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (errToken, user) => {
+      if (errToken) return res.status(403).json(errToken);
+
+      const userId = userFound._id.toString();
+      const accessToken = generateAccessToken({ id: userId });
+      return res.status(200).json({ accessToken: accessToken });
+    })
+  })
+}
+
+export const logout = (req, res) => {
+  return res
+  .status(200)
+  .cookie('refreshToken', '', { httpOnly: true, secure: true, sameSite: 'strict', maxAge: -1 })
+  .json({ message: 'logout successful' });
+}
---
+++ middlewares/auth.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const authenticateUser = (req, res, next) => {
+    console.log('bloup3');
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token === null) return res.status(401).json({ error: 'Unauthorized' });
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        
+        if (err) return res.status(401).json('Unauthorized');
+
+        req.user = user;
+        next();
+    })
+}
+
+// export const autenticateAdmin = (req, res, next) => {
+//     const authHeader = req.headers['authorization'];
+//     const token = authHeader && authHeader.split(' ')[1];
+
+//     if (token === null) res.status(401).json({ error: 'Unauthorized' });
+
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//         if (err) res.status(401).json('Unauthorized');
+
+//         req.user = user;
+//         next();
+//     })
+// }
