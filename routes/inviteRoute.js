6c4b3a4988679145cac7d182c7ecafa4fe784c84
Camille100 <76488507+Camille100@users.noreply.github.com>
2022-12-06 17:43:49 +0100

    adding getInvitesByUser route

--- routes/inviteRoute.js
+++ routes/inviteRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addInvite, getInvites, getInvite, updateInvite, deleteInvite } from '../controllers/inviteController.js';
+import { addInvite, getInvites, getInvitesByUser, getInvite, updateInvite, deleteInvite } from '../controllers/inviteController.js';
 
 const router = express.Router();
 
 router.post('/add', addInvite);
 router.get('/invites', getInvites);
+router.get('/invites/:userId', getInvitesByUser);
 router.get('/:inviteId', getInvite);
 router.put('/update/:inviteId', updateInvite);
 router.delete('/delete/:inviteId', deleteInvite);
