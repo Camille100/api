192aecb8cb77edcea9c1b4630982cff5de0c954a
Camille100 <76488507+Camille100@users.noreply.github.com>
2022-12-06 17:44:02 +0100

    adding getNotificationsByUser route

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addNotification, getNotification, getNotifications, updateNotification, deleteNotification } from '../controllers/notificationController.js';
+import { addNotification, getNotification, getNotifications, getNotificationsByUser, updateNotification, deleteNotification } from '../controllers/notificationController.js';
 
 const router = express.Router();
 
 router.post('/add', addNotification);
 router.get('/notifications', getNotifications);
+router.get('/notifications/:userId', getNotificationsByUser);
 router.get('/:notificationId', getNotification);
 router.put('/update/:notificationId', updateNotification);
 router.delete('/delete/:notificationId', deleteNotification);
