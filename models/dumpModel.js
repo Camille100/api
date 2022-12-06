3db5a49f9009e53f9d5d050ea8d9c7f7355577b5
Camille100 <76488507+Camille100@users.noreply.github.com>
2022-12-06 17:43:05 +0100

    fix dumpModel

--- models/dumpModel.js
+++ models/dumpModel.js
@@ -2,69 +2,69 @@ import mongoose, { Schema } from 'mongoose';
 import { pointSchema } from './pointModel.js';
 
 const dumpSchema = new Schema({
-    creator: {
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  cleaningDemands: [
+    {
+      cleaner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true
-    },
-    cleaningDemands: [
+        required: true,
+      },
+      status: {
+        type: String,
+        default: 'waiting',
+        enum: ['waiting', 'accepted', 'refused']
+      },
+      pictures: [
         {
-            cleaner: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User',
-                required: true,
-            },
-            status: {
-                type: String,
-                default: 'waiting',
-                enum: ['waiting', 'accepted', 'refused']
-            },
-            pictures: [
-                {
-                    type: String
-                }
-            ]
+          type: String
         }
-    ],
-    cleaner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    comment: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        default: 'open',
-        enum: ['open', 'waiting', 'closed'],
-        required: true,
-    },
-    equipments: [
-        { type: mongoose.Schema.Types.ObjectId, ref: 'Equipment' }
-    ],
-    accessible: {
-        onFoot: {
-            type: Boolean,
-            default: false,
-        },
-        onCar: {
-            type: Boolean,
-            default: false,
-        }
-    },
-    location: {
-        type: pointSchema,
-        required: true
-    },
-    pictures: [
-        { type: "String", required: true }
-    ]
-}, {
-    timestamps: {
-        createdAt: 'created_at',
-        updatedAt: 'updated_at'
+      ]
     }
+  ],
+  cleaner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: 'open',
+    enum: ['open', 'waiting', 'closed'],
+    required: true,
+  },
+  equipments: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Equipment' }
+  ],
+  accessible: {
+    onFoot: {
+      type: Boolean,
+      default: false,
+    },
+    onCar: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  location: {
+    type: pointSchema,
+    required: true
+  },
+  pictures: [
+    { type: "String", required: true }
+  ]
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
 });
 
 const Dump = mongoose.model('Dump', dumpSchema);
