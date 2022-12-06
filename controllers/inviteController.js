b92c4213321a12b4614f79a1f3912be487b26859
Camille100 <76488507+Camille100@users.noreply.github.com>
2022-12-06 17:41:28 +0100

    addInvite + getInvites + getIncite + updateInvite

--- controllers/inviteController.js
+++ controllers/inviteController.js
@@ -1,56 +1,135 @@
 /* eslint-disable no-unused-vars */
 import dotenv from 'dotenv';
 import Invite from '../models/inviteModel.js';
+import Event from '../models/eventModel.js';
+import { sendNotification } from './notificationController.js';
 
 dotenv.config();
 
 export const addInvite = async (req, res) => {
 
   const invite = new Invite({
+    status: req.body.status,
+    idSender: req.body.idSender,
+    idReceiver: req.body.idReceiver,
+    idEvent: req.body.idEvent
   });
 
   invite.save()
     .then((response) => {
-      return res.status(201).json(response);
+      const data = {
+        message: 'Vous avez reçu une invitation à un évènement',
+        idUser: [req.body.idReceiver],
+        idEvent: req.body.idEvent
+      }
+      sendNotification(data).then(() => {
+        return res.status(200).json(response);
+      })
     })
     .catch((err) => res.status(400).json(err));
 }
 
 export const getInvite = async (req, res) => {
-    Invite.findOne({ '_id': req.body.inviteId })
+  Invite.findOne({ '_id': req.body.inviteId })
     .lean()
     .exec((err, invite) => {
-        if (err) return res.status(400).json(err);
-        else if (invite === null) {
-            return res.status(400).json({ error: 'No invite found' });
-        }
-        else {
-            return res.status(200).json(invite);
-        }
+      if (err) return res.status(400).json(err);
+      else if (invite === null) {
+        return res.status(400).json({ error: 'No invite found' });
+      }
+      else {
+        return res.status(200).json(invite);
+      }
     })
 }
 
 export const getInvites = (req, res) => {
-    Invite.find()
+  Invite.find()
     .lean()
     .exec((err, invites) => {
-        if (err) return res.status(400).json(err);
-        else if (invites.length === 0) {
-            return res.status(400).json({ error: 'No invites found' });
-        }
-        else {
-            return res.status(200).json(invites);
-        }
+      if (err) return res.status(400).json(err);
+      else if (invites.length === 0) {
+        return res.status(400).json({ error: 'No invites found' });
+      }
+      else {
+        return res.status(200).json(invites);
+      }
+    })
+}
+
+export const getInvitesByUser = (req, res) => {
+  Invite.find({
+    $or: [
+      { idSender: req.params.userId },
+      { idReceiver: req.params.userId }
+    ]
+  })
+    .populate([
+      {
+        path: 'idEvent',
+        select: 'beginDate endDate',
+      },
+      {
+        path: 'idSender',
+        select: 'email pseudo'
+      }
+    ])
+    .lean()
+    .exec((err, invites) => {
+      if (err) return res.status(400).json(err);
+      else if (invites.length === 0) {
+        return res.status(400).json({ error: 'No invites found' });
+      }
+      else {
+        return res.status(200).json(invites);
+      }
     })
 }
 
 export const updateInvite = (req, res) => {
-    Invite.updateOne({ _id: req.body.inviteId }, { $set: { ...req.body.inviteData } })
-    .exec((err, updated) => {
-        if (err) return res.status(400).json(err);
-        else {
-            return res.status(200).json({ message: 'Invite updated' });
+  Invite.findOneAndUpdate(
+    { _id: req.params.inviteId },
+    { ...req.body },
+    { new: true }
+  )
+    .populate([
+      {
+        path: 'idReceiver',
+        select: 'email'
+      }
+    ])
+    .lean()
+    .exec((err, updatedInvite) => {
+      if (err) return res.status(400).json(err);
+      else {
+        if (req.body.status && req.body.status === 'accepted') {
+          Event.findOneAndUpdate(
+            { _id: updatedInvite.idEvent },
+            { $push: { participants: { userId: updatedInvite.idReceiver } } }
+          )
+            .lean()
+            .exec((error, updatedEvent) => {
+              if (error) return res.status(400).json(error);
+              const data = {
+                message: `L'invitation que vous avez envoyé à ${updatedInvite.idReceiver.email} a été acceptée`,
+                idUser: [updatedInvite.idSender],
+                idEvent: updatedEvent._id
+              }
+              sendNotification(data).then(() => {
+                return res.status(200).json({ message: 'Invite accepted' });
+              })
+            })
+        } else {
+          const data = {
+            message: `L'invitation que vous avez envoyé à ${updatedInvite.idReceiver.email} a été refusée`,
+            idUser: [updatedInvite.idSender],
+            idEvent: updatedInvite.idEvent
+          }
+          sendNotification(data).then(() => {
+            return res.status(200).json({ message: 'Invite refused' });
+          })
         }
+      }
     })
 }
 
