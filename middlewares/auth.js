5f8e52b1cc8e2aba8731ecfdf39bd1f79257cc3e
Camille100 <76488507+Camille100@users.noreply.github.com>
2022-12-06 17:42:45 +0100

    fix auth status codes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,11 +7,11 @@ export const authenticateUser = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token === null) return res.status(401).json({ error: 'Unauthorized' });
+    if (token === null) return res.status(403).json({ error: 'Unauthorized' });
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         
-        if (err) return res.status(401).json('Unauthorized');
+        if (err) return res.status(403).json('Unauthorized');
 
         req.user = user;
         next();
